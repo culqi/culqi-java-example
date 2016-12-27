e95526493289a58290c5e2df5d2aeb26d352bc85
Willy Aguirre <marti1125@gmail.com>
2016-12-27 10:40:55 -0500

    update example

--- src/main/java/com/culqi/example/controller/Server.java
+++ src/main/java/com/culqi/example/controller/Server.java
@@ -16,10 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class Server {
 
     @RequestMapping(value= "/charges", method = RequestMethod.POST, produces = "application/json")
-    public String charges(@RequestParam("token") String token) throws Exception {
+    public String charges(@RequestParam("token") String token, @RequestParam("first_name") String first_name, @RequestParam("last_name") String last_name,  @RequestParam("email") String email) throws Exception {
         Culqi culqi = new Culqi("pk_test_vzMuTHoueOMlgUPj","sk_test_UTCQSGcXW8bCyU59");
-        Result result = culqi.createCharge("Avenida Lima 1232", "LIMA", 1000, "PE", "PEN", "123", "waguirre@me.com",
-                "Willy", 0, "Aguirre", "", new Util().ramdomNumber(), 3333339, "Venta de prueba", token);
+        Result result = culqi.createCharge("Avenida Lima 1232", "LIMA", 1000, "PE", "PEN", "123", email,
+                first_name, 0, last_name, "", new Util().ramdomNumber(), 3333339, "Venta de prueba", token);
         return "{\"status\":\"" + result.getStatus() + "\",\"id\":\"" + result.getId() + "\",\"message\":\""+result.getMessage()+"\"}";
     }
 
